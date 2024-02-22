0f720ff9d83ff25fc4ae0e73915ea6640793b644
PsicoThePato <jmomcontato@gmail.com>
2024-02-22 02:11:12 -0300

    added argparser

--- operations.py
+++ operations.py
@@ -1,11 +1,13 @@
+import argparse
+from typing import Any, Iterable
+
 from communex.client import CommuneClient
 from communex.compat.key import classic_load_key
-from substrateinterface import (ExtrinsicReceipt, Keypair)  # type: ignore
-from typing import Any
-from communex.types import NetworkParams
-from communex.types import Ss58Address
+from communex.types import NetworkParams, Ss58Address
+from substrateinterface import Keypair  # type: ignore
+from substrateinterface.base import ExtrinsicReceipt  # type: ignore
 
-
+# shouldn't this be passed via cli/file?
 params: NetworkParams = {
     "max_allowed_subnets": 256,
     "max_allowed_modules": 10000,
@@ -19,8 +21,9 @@ params: NetworkParams = {
     "burn_rate": 0,
     "min_burn": 100_000_000_000,
     "min_stake": 0,
-    "min_weight_stake": 0
-} 
+    "min_weight_stake": 0,
+}
+
 
 def update_global_multisig(
     client: CommuneClient,
@@ -35,7 +38,7 @@ def update_global_multisig(
     general_params = dict(params)
 
     response = client.compose_call_multisig(
-        fn='update_global',
+        fn="update_global",
         module="SubspaceModule",
         params=general_params,
         signatories=signatories,
@@ -43,28 +46,116 @@ def update_global_multisig(
         key=key,
         sudo=True,
     )
-
     return response
 
 
+
+def update_params(
+    client: CommuneClient,
+    key: Keypair,
+    signatories: list[Ss58Address],
+    threshold: int,
+    params: Any,
+    ):
+    response = client.compose_call_multisig(
+    fn="update_global",
+    module="SubspaceModule",
+    params=params,
+    signatories=signatories,
+    threshold=threshold,
+    key=key,
+    sudo=True,
+    )
+    return response
+
+
+def runtime_upgrade(
+        client: CommuneClient,
+        key: Keypair,
+        signatories: list[Ss58Address],
+        threshold: int,
+        wasm_path: Any,
+    ):
+    cont = input(
+        f"Will upgrade the wasm to {wasm_path} file. Are you sure? (y/n)"
+        )
+    if cont != "y":
+        print("Aborted.")
+        exit(0)
+    with open(wasm_path, "rb") as file:
+        wasm = file.read()
+
+    response = client.compose_call_multisig(
+    fn="setCode",
+    module="system",
+    params=wasm,
+    signatories=signatories,
+    threshold=threshold,
+    key=key,
+    sudo=True,
+    )
+    return response
+
+def config_parser(rpc_choices: Iterable[str]):
+    parser = argparse.ArgumentParser(description="Update global multisig")
+    parser.add_argument("keyname", help="Name of the key to use")
+    parser.add_argument(
+        "threshold", 
+        help="minimum number of signatures", 
+        type=int
+    )
+    parser.add_argument(
+        "function", 
+        help="rpc call to execute",
+        choices=rpc_choices,
+    )
+    parser.add_argument(
+        "--wasm-path", help="path to the wasm file", required=False
+        )
+    
+    args = parser.parse_args()
+    
+    return args
+
+
 if __name__ == "__main__":
-    client = CommuneClient(url="wss://commune.api.onfinality.io/public-ws")
 
-    my_multisig = classic_load_key("ronaldo")
+    rpc_map = {
+        "runtime_upgrade": runtime_upgrade,
+        "update_params": update_params,
+    }
 
-    threshold = 3
+    args = config_parser(rpc_map.keys())
+    ronaldo_key = args.keyname
+    rpc_method = args.function
+    rpc_func = rpc_map[rpc_method]
+    threshold = args.threshold
+    wasm_path = args.wasm_path
 
-    signatories: list[Ss58Address] = [
-        "5ELSoV9ntKSgjLQ2UQzUqkvQnpGoJyHWjo4cSp2w5yiEuSwW",  # Ho
-        "5GWDrAW9sUTAwB53wjBYyBUngncnGww7b4GAnaGPp7PjRpQD",  # Ti
-        "5HHPz5GdvSCw3WNZWVGy6ejgqwVA3oAKUPeaqw9aEDxDokaS",  # Hu
-        "5E49Ry24q9JwB9AvUR2igfJHeqsDXmPuXRzC6jBqdHEgpE1X",  # Fa
-        "5FHqJ94yptoK4ELSBqvJV4k3PKJXLsRUrKpVc6VFLUBgUZfG",  # Co
+    if rpc_method == "runtime_upgrade":
+        assert wasm_path, "wasm path is required for runtime_upgrade"
+    
+    my_multisig = classic_load_key(ronaldo_key)
+    client = CommuneClient(url="wss://localhost:9944")
+
+    # signatories: list[Ss58Address] = [
+    #     "5ELSoV9ntKSgjLQ2UQzUqkvQnpGoJyHWjo4cSp2w5yiEuSwW",  # Ho
+    #     "5GWDrAW9sUTAwB53wjBYyBUngncnGww7b4GAnaGPp7PjRpQD",  # Ti
+    #     "5HHPz5GdvSCw3WNZWVGy6ejgqwVA3oAKUPeaqw9aEDxDokaS",  # Hu
+    #     "5E49Ry24q9JwB9AvUR2igfJHeqsDXmPuXRzC6jBqdHEgpE1X",  # Fa
+    #     "5FHqJ94yptoK4ELSBqvJV4k3PKJXLsRUrKpVc6VFLUBgUZfG",  # Co
+    # ]
+
+    multi_sig_addr: list[Ss58Address] = [
+        "5FRE6GqpW1G6o65kk3ib2Ea28Ah4FR6F3EeZJfmBwd3XwHXq",
+        "5EZJYuTFdkzkLZew7Tnm7phuZrejHBks4XPz3UDZdMh11ALA",
+        "5D4oWCPSTBT2ZyQAy8b4xqrNzmUQfARr6ZjN3zr62eyqDz36",
     ]
 
     input(f"enter if you agree with the parameters ? {params}")
 
-    result = (update_global_multisig(
-        client, my_multisig, signatories, threshold, params))
+    result = rpc_func(
+        client, my_multisig, multi_sig_addr, threshold, params
+    )
 
     print(f"it ended up like this: {result}")
